db73926aac821b0e0b4a712d2603dd0544511fbb
PetrosVal <petrosva12gmail.com>
2021-02-11 18:57:41 +0200

    Ad ApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+    def get(self,request,format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Users HTTP as function (get,post,patch,delete,put)',
+            'Gives you the most control of your application logic',
+            'It is mapped manually to URL',
+        ]
+
+        return Response({'message:' 'Hello', 'apiview:' an_apiview})
